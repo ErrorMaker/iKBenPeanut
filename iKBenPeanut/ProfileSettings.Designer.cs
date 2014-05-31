9078ec07d2551b183c93ce0cc4a7d0a558b21e70
TheOldNutMan <AgedPeanuts@gmail.com>
2014-05-30 23:19:08 -0700

    Implemented asynchronous methods from the SKore library to grab/update the authenticated account's data.

--- iKBenPeanut/ProfileSettings.Designer.cs
+++ iKBenPeanut/ProfileSettings.Designer.cs
@@ -1,6 +1,6 @@
-﻿namespace Sulakore
+﻿namespace iKBenPeanut
 {
-    partial class Settings
+    partial class ProfileSettings
     {
         /// <summary>
         /// Required designer variable.
@@ -28,22 +28,32 @@
         /// </summary>
         private void InitializeComponent()
         {
-            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Settings));
-            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(ProfileSettings));
             this.AvatarPctbx = new System.Windows.Forms.PictureBox();
-            this.groupBox1.SuspendLayout();
+            this.MottoGrpbx = new System.Windows.Forms.GroupBox();
+            this.MottoTxt = new System.Windows.Forms.TextBox();
+            this.ClothesGrpbx = new System.Windows.Forms.GroupBox();
+            this.IsMaleChckbx = new System.Windows.Forms.CheckBox();
+            this.ClothesTxt = new System.Windows.Forms.TextBox();
+            this.AllowFRChckbx = new System.Windows.Forms.CheckBox();
+            this.OfflineMessagingChckbx = new System.Windows.Forms.CheckBox();
+            this.WCFMGrpbx = new System.Windows.Forms.GroupBox();
+            this.WCFMMyFriendsRdio = new System.Windows.Forms.RadioButton();
+            this.WCFMNobodyRdio = new System.Windows.Forms.RadioButton();
+            this.OSVisibilityGrpbx = new System.Windows.Forms.GroupBox();
+            this.OSEveryoneRdio = new System.Windows.Forms.RadioButton();
+            this.OSNobodyRdio = new System.Windows.Forms.RadioButton();
+            this.HPVisibilityGrpbx = new System.Windows.Forms.GroupBox();
+            this.HPEveryoneRdio = new System.Windows.Forms.RadioButton();
+            this.HPNobodyRdio = new System.Windows.Forms.RadioButton();
+            this.UpdateProfileBtn = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.AvatarPctbx)).BeginInit();
+            this.MottoGrpbx.SuspendLayout();
+            this.ClothesGrpbx.SuspendLayout();
+            this.WCFMGrpbx.SuspendLayout();
+            this.OSVisibilityGrpbx.SuspendLayout();
+            this.HPVisibilityGrpbx.SuspendLayout();
             this.SuspendLayout();
-            // 
-            // groupBox1
-            // 
-            this.groupBox1.Controls.Add(this.AvatarPctbx);
-            this.groupBox1.Location = new System.Drawing.Point(13, 13);
-            this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(76, 174);
-            this.groupBox1.TabIndex = 0;
-            this.groupBox1.TabStop = false;
-            this.groupBox1.Text = "Avatar";
             // 
             // AvatarPctbx
             // 
@@ -53,34 +63,254 @@
             this.AvatarPctbx.BackColor = System.Drawing.Color.Transparent;
             this.AvatarPctbx.ErrorImage = null;
             this.AvatarPctbx.InitialImage = null;
-            this.AvatarPctbx.Location = new System.Drawing.Point(6, 19);
+            this.AvatarPctbx.Location = new System.Drawing.Point(12, 12);
             this.AvatarPctbx.Name = "AvatarPctbx";
-            this.AvatarPctbx.Size = new System.Drawing.Size(64, 149);
+            this.AvatarPctbx.Size = new System.Drawing.Size(65, 110);
+            this.AvatarPctbx.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
             this.AvatarPctbx.TabIndex = 1;
             this.AvatarPctbx.TabStop = false;
             // 
-            // Settings
+            // MottoGrpbx
+            // 
+            this.MottoGrpbx.Controls.Add(this.MottoTxt);
+            this.MottoGrpbx.Location = new System.Drawing.Point(82, 12);
+            this.MottoGrpbx.Name = "MottoGrpbx";
+            this.MottoGrpbx.Size = new System.Drawing.Size(248, 50);
+            this.MottoGrpbx.TabIndex = 1;
+            this.MottoGrpbx.TabStop = false;
+            this.MottoGrpbx.Text = "Motto";
+            // 
+            // MottoTxt
+            // 
+            this.MottoTxt.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.MottoTxt.Location = new System.Drawing.Point(6, 18);
+            this.MottoTxt.MaxLength = 38;
+            this.MottoTxt.Name = "MottoTxt";
+            this.MottoTxt.Size = new System.Drawing.Size(236, 20);
+            this.MottoTxt.TabIndex = 2;
+            this.MottoTxt.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // ClothesGrpbx
+            // 
+            this.ClothesGrpbx.Controls.Add(this.IsMaleChckbx);
+            this.ClothesGrpbx.Controls.Add(this.ClothesTxt);
+            this.ClothesGrpbx.Location = new System.Drawing.Point(82, 72);
+            this.ClothesGrpbx.Name = "ClothesGrpbx";
+            this.ClothesGrpbx.Size = new System.Drawing.Size(248, 50);
+            this.ClothesGrpbx.TabIndex = 3;
+            this.ClothesGrpbx.TabStop = false;
+            this.ClothesGrpbx.Text = "Clothes";
+            // 
+            // IsMaleChckbx
+            // 
+            this.IsMaleChckbx.AutoSize = true;
+            this.IsMaleChckbx.Checked = true;
+            this.IsMaleChckbx.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.IsMaleChckbx.Location = new System.Drawing.Point(180, 0);
+            this.IsMaleChckbx.Name = "IsMaleChckbx";
+            this.IsMaleChckbx.Size = new System.Drawing.Size(57, 17);
+            this.IsMaleChckbx.TabIndex = 14;
+            this.IsMaleChckbx.Text = "IsMale";
+            this.IsMaleChckbx.UseVisualStyleBackColor = true;
+            // 
+            // ClothesTxt
+            // 
+            this.ClothesTxt.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.ClothesTxt.Location = new System.Drawing.Point(6, 18);
+            this.ClothesTxt.Name = "ClothesTxt";
+            this.ClothesTxt.Size = new System.Drawing.Size(236, 20);
+            this.ClothesTxt.TabIndex = 2;
+            this.ClothesTxt.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // AllowFRChckbx
+            // 
+            this.AllowFRChckbx.AutoSize = true;
+            this.AllowFRChckbx.Location = new System.Drawing.Point(193, 206);
+            this.AllowFRChckbx.Name = "AllowFRChckbx";
+            this.AllowFRChckbx.Size = new System.Drawing.Size(126, 17);
+            this.AllowFRChckbx.TabIndex = 10;
+            this.AllowFRChckbx.Text = "Allow Friend Request";
+            this.AllowFRChckbx.UseVisualStyleBackColor = true;
+            // 
+            // OfflineMessagingChckbx
+            // 
+            this.OfflineMessagingChckbx.AutoSize = true;
+            this.OfflineMessagingChckbx.Location = new System.Drawing.Point(193, 191);
+            this.OfflineMessagingChckbx.Name = "OfflineMessagingChckbx";
+            this.OfflineMessagingChckbx.Size = new System.Drawing.Size(110, 17);
+            this.OfflineMessagingChckbx.TabIndex = 12;
+            this.OfflineMessagingChckbx.Text = "Offline Messaging";
+            this.OfflineMessagingChckbx.UseVisualStyleBackColor = true;
+            // 
+            // WCFMGrpbx
+            // 
+            this.WCFMGrpbx.Controls.Add(this.WCFMMyFriendsRdio);
+            this.WCFMGrpbx.Controls.Add(this.WCFMNobodyRdio);
+            this.WCFMGrpbx.Location = new System.Drawing.Point(174, 128);
+            this.WCFMGrpbx.Name = "WCFMGrpbx";
+            this.WCFMGrpbx.Size = new System.Drawing.Size(156, 47);
+            this.WCFMGrpbx.TabIndex = 13;
+            this.WCFMGrpbx.TabStop = false;
+            this.WCFMGrpbx.Text = "Who Can Follow Me";
+            // 
+            // WCFMMyFriendsRdio
+            // 
+            this.WCFMMyFriendsRdio.AutoSize = true;
+            this.WCFMMyFriendsRdio.Location = new System.Drawing.Point(74, 19);
+            this.WCFMMyFriendsRdio.Name = "WCFMMyFriendsRdio";
+            this.WCFMMyFriendsRdio.Size = new System.Drawing.Size(76, 17);
+            this.WCFMMyFriendsRdio.TabIndex = 6;
+            this.WCFMMyFriendsRdio.Text = "My Friends";
+            this.WCFMMyFriendsRdio.UseVisualStyleBackColor = true;
+            // 
+            // WCFMNobodyRdio
+            // 
+            this.WCFMNobodyRdio.AutoSize = true;
+            this.WCFMNobodyRdio.Checked = true;
+            this.WCFMNobodyRdio.Location = new System.Drawing.Point(6, 19);
+            this.WCFMNobodyRdio.Name = "WCFMNobodyRdio";
+            this.WCFMNobodyRdio.Size = new System.Drawing.Size(62, 17);
+            this.WCFMNobodyRdio.TabIndex = 5;
+            this.WCFMNobodyRdio.TabStop = true;
+            this.WCFMNobodyRdio.Text = "Nobody";
+            this.WCFMNobodyRdio.UseVisualStyleBackColor = true;
+            // 
+            // OSVisibilityGrpbx
+            // 
+            this.OSVisibilityGrpbx.Controls.Add(this.OSEveryoneRdio);
+            this.OSVisibilityGrpbx.Controls.Add(this.OSNobodyRdio);
+            this.OSVisibilityGrpbx.Location = new System.Drawing.Point(12, 181);
+            this.OSVisibilityGrpbx.Name = "OSVisibilityGrpbx";
+            this.OSVisibilityGrpbx.Size = new System.Drawing.Size(156, 47);
+            this.OSVisibilityGrpbx.TabIndex = 11;
+            this.OSVisibilityGrpbx.TabStop = false;
+            this.OSVisibilityGrpbx.Text = "Online Status Visibility";
+            // 
+            // OSEveryoneRdio
+            // 
+            this.OSEveryoneRdio.AutoSize = true;
+            this.OSEveryoneRdio.Location = new System.Drawing.Point(80, 19);
+            this.OSEveryoneRdio.Name = "OSEveryoneRdio";
+            this.OSEveryoneRdio.Size = new System.Drawing.Size(70, 17);
+            this.OSEveryoneRdio.TabIndex = 6;
+            this.OSEveryoneRdio.Text = "Everyone";
+            this.OSEveryoneRdio.UseVisualStyleBackColor = true;
+            // 
+            // OSNobodyRdio
+            // 
+            this.OSNobodyRdio.AutoSize = true;
+            this.OSNobodyRdio.Checked = true;
+            this.OSNobodyRdio.Location = new System.Drawing.Point(9, 19);
+            this.OSNobodyRdio.Name = "OSNobodyRdio";
+            this.OSNobodyRdio.Size = new System.Drawing.Size(62, 17);
+            this.OSNobodyRdio.TabIndex = 5;
+            this.OSNobodyRdio.TabStop = true;
+            this.OSNobodyRdio.Text = "Nobody";
+            this.OSNobodyRdio.UseVisualStyleBackColor = true;
+            // 
+            // HPVisibilityGrpbx
+            // 
+            this.HPVisibilityGrpbx.Controls.Add(this.HPEveryoneRdio);
+            this.HPVisibilityGrpbx.Controls.Add(this.HPNobodyRdio);
+            this.HPVisibilityGrpbx.Location = new System.Drawing.Point(12, 128);
+            this.HPVisibilityGrpbx.Name = "HPVisibilityGrpbx";
+            this.HPVisibilityGrpbx.Size = new System.Drawing.Size(156, 47);
+            this.HPVisibilityGrpbx.TabIndex = 9;
+            this.HPVisibilityGrpbx.TabStop = false;
+            this.HPVisibilityGrpbx.Text = "Homepage Visibility";
+            // 
+            // HPEveryoneRdio
+            // 
+            this.HPEveryoneRdio.AutoSize = true;
+            this.HPEveryoneRdio.Location = new System.Drawing.Point(80, 19);
+            this.HPEveryoneRdio.Name = "HPEveryoneRdio";
+            this.HPEveryoneRdio.Size = new System.Drawing.Size(70, 17);
+            this.HPEveryoneRdio.TabIndex = 6;
+            this.HPEveryoneRdio.Text = "Everyone";
+            this.HPEveryoneRdio.UseVisualStyleBackColor = true;
+            // 
+            // HPNobodyRdio
+            // 
+            this.HPNobodyRdio.AutoSize = true;
+            this.HPNobodyRdio.Checked = true;
+            this.HPNobodyRdio.Location = new System.Drawing.Point(9, 19);
+            this.HPNobodyRdio.Name = "HPNobodyRdio";
+            this.HPNobodyRdio.Size = new System.Drawing.Size(62, 17);
+            this.HPNobodyRdio.TabIndex = 5;
+            this.HPNobodyRdio.TabStop = true;
+            this.HPNobodyRdio.Text = "Nobody";
+            this.HPNobodyRdio.UseVisualStyleBackColor = true;
+            // 
+            // UpdateProfileBtn
+            // 
+            this.UpdateProfileBtn.Location = new System.Drawing.Point(12, 234);
+            this.UpdateProfileBtn.Name = "UpdateProfileBtn";
+            this.UpdateProfileBtn.Size = new System.Drawing.Size(318, 23);
+            this.UpdateProfileBtn.TabIndex = 14;
+            this.UpdateProfileBtn.Text = "Update Profile";
+            this.UpdateProfileBtn.UseVisualStyleBackColor = true;
+            this.UpdateProfileBtn.Click += new System.EventHandler(this.UpdateProfileBtn_Click);
+            // 
+            // ProfileSettings
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.White;
-            this.ClientSize = new System.Drawing.Size(470, 369);
-            this.Controls.Add(this.groupBox1);
+            this.ClientSize = new System.Drawing.Size(342, 266);
+            this.Controls.Add(this.UpdateProfileBtn);
+            this.Controls.Add(this.AllowFRChckbx);
+            this.Controls.Add(this.OfflineMessagingChckbx);
+            this.Controls.Add(this.WCFMGrpbx);
+            this.Controls.Add(this.OSVisibilityGrpbx);
+            this.Controls.Add(this.HPVisibilityGrpbx);
+            this.Controls.Add(this.ClothesGrpbx);
+            this.Controls.Add(this.AvatarPctbx);
+            this.Controls.Add(this.MottoGrpbx);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.MaximizeBox = false;
-            this.Name = "Settings";
+            this.Name = "ProfileSettings";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
-            this.Text = "iKBenPeanut ~ Profile Settings | 0";
-            this.groupBox1.ResumeLayout(false);
+            this.Text = "iKBenPeanut ~ Profile Settings";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.ProfileSettings_FormClosed);
+            this.Load += new System.EventHandler(this.ProfileSettings_Load);
             ((System.ComponentModel.ISupportInitialize)(this.AvatarPctbx)).EndInit();
+            this.MottoGrpbx.ResumeLayout(false);
+            this.MottoGrpbx.PerformLayout();
+            this.ClothesGrpbx.ResumeLayout(false);
+            this.ClothesGrpbx.PerformLayout();
+            this.WCFMGrpbx.ResumeLayout(false);
+            this.WCFMGrpbx.PerformLayout();
+            this.OSVisibilityGrpbx.ResumeLayout(false);
+            this.OSVisibilityGrpbx.PerformLayout();
+            this.HPVisibilityGrpbx.ResumeLayout(false);
+            this.HPVisibilityGrpbx.PerformLayout();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
-        private System.Windows.Forms.GroupBox groupBox1;
         private System.Windows.Forms.PictureBox AvatarPctbx;
+        private System.Windows.Forms.GroupBox MottoGrpbx;
+        private System.Windows.Forms.TextBox MottoTxt;
+        private System.Windows.Forms.GroupBox ClothesGrpbx;
+        private System.Windows.Forms.TextBox ClothesTxt;
+        private System.Windows.Forms.CheckBox IsMaleChckbx;
+        private System.Windows.Forms.CheckBox AllowFRChckbx;
+        private System.Windows.Forms.CheckBox OfflineMessagingChckbx;
+        private System.Windows.Forms.GroupBox WCFMGrpbx;
+        private System.Windows.Forms.RadioButton WCFMMyFriendsRdio;
+        private System.Windows.Forms.RadioButton WCFMNobodyRdio;
+        private System.Windows.Forms.GroupBox OSVisibilityGrpbx;
+        private System.Windows.Forms.RadioButton OSEveryoneRdio;
+        private System.Windows.Forms.RadioButton OSNobodyRdio;
+        private System.Windows.Forms.GroupBox HPVisibilityGrpbx;
+        private System.Windows.Forms.RadioButton HPEveryoneRdio;
+        private System.Windows.Forms.RadioButton HPNobodyRdio;
+        private System.Windows.Forms.Button UpdateProfileBtn;
     }
 }
--- iKBenPeanut/ProfileSettings.cs
+++ iKBenPeanut/ProfileSettings.cs
@@ -1,19 +1,61 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
+using Sulakore;
 using System.Drawing;
-using System.Linq;
-using System.Text;
 using System.Windows.Forms;
 
-namespace Sulakore
+namespace iKBenPeanut
 {
-    public partial class Settings : Form
+    public partial class ProfileSettings : Form
     {
-        public Settings()
+        private Main MainF;
+        public ProfileSettings(Main MainF)
         {
+            this.MainF = MainF;
             InitializeComponent();
+        }
+        private void ProfileSettings_Load(object sender, System.EventArgs e)
+        {
+            SKore.BeginGetPlayerAvatar(Program.Account.PlayerName, Program.Account.Hotel, new AsyncCallback((iAr) =>
+            {
+                Image PA = SKore.EndGetPlayerAvatar(iAr);
+                Invoke(new Action(() => { AvatarPctbx.Image = PA; }));
+            }), null);
+            SKore.BeginGetPlayerMotto(Program.Account.PlayerName, Program.Account.Hotel, new AsyncCallback((iAr) =>
+            {
+                string PM = SKore.EndGetPlayerMotto(iAr);
+                Invoke(new Action(() => { MottoTxt.Text = PM; }));
+            }), null);
+            SKore.BeginGetPlayerClothes(Program.Account.PlayerName, Program.Account.Hotel, new AsyncCallback((iAr) =>
+            {
+                string PC = SKore.EndGetPlayerClothes(iAr);
+                Invoke(new Action(() => { ClothesTxt.Text = PC; }));
+            }), null);
+        }
+
+        private void UpdateProfileBtn_Click(object sender, EventArgs e)
+        {
+            string CI = ClothesTxt.Text;
+            UpdateProfileBtn.Enabled = false;
+            Program.Account.BeginSaveOutfit(CI, IsMaleChckbx.Checked ? HGenders.Male : HGenders.Female, new AsyncCallback((iAr) =>
+            {
+                Program.Account.EndSaveOutfit(iAr);
+                object[] o = (iAr.AsyncState as object[]);
+                Program.Account.BeginUpdateProfile((string)o[0], (bool)o[1], (bool)o[2], (bool)o[3], (bool)o[4], (bool)o[5], new AsyncCallback((ar) =>
+                {
+                    Program.Account.EndUpdateProfile(ar);
+                    SKore.BeginGetPlayerAvatar(CI, new AsyncCallback((iar) =>
+                    {
+                        Image I = SKore.EndGetPlayerAvatar(iar);
+                        Invoke(new Action(() => { AvatarPctbx.Image = I; })); 
+                    }), null);
+                    Invoke(new Action(() => { UpdateProfileBtn.Enabled = true; }));
+                }), null);
+            }), new object[6] { MottoTxt.Text, HPEveryoneRdio.Checked, AllowFRChckbx.Checked, OSEveryoneRdio.Checked, OfflineMessagingChckbx.Checked, WCFMMyFriendsRdio.Checked });
+        }
+
+        private void ProfileSettings_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            MainF.PSettings = null;
         }
     }
 }
